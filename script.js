22d5879409c4050ef890033615051d557ae94c49
Chhaya <117908142+ChhayaSengar@users.noreply.github.com>
2023-04-14 13:54:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructot(species){
+		this.species = species;
+	}
+	makeSound()[
+		console.log("sound the animal makes.")
+	]
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	bark(){
+		console.log("woof")
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	purr(){
+		console.log("purr")
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
